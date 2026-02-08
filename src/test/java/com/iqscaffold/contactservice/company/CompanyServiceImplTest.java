18f6d491661fe89c5d910770433e02798944d604
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-02-08 16:57:22 +0200

    style: fix and remove unused imports

--- src/test/java/com/iqscaffold/contactservice/company/CompanyServiceImplTest.java
+++ src/test/java/com/iqscaffold/contactservice/company/CompanyServiceImplTest.java
@@ -22,7 +22,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 @ExtendWith(MockitoExtension.class)
 class CompanyServiceImplTest {
